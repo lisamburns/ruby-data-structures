e9915f1117623df9ed86003682e0c16cfbdc2b80
App Academy Student <student@appacademy.io>
2015-09-28 12:15:25 -0700

    Complete heapify down with proc, push and extract methods

--- code/lib/03_heap.rb
+++ code/lib/03_heap.rb
@@ -1,10 +1,11 @@
 require 'byebug'
 class BinaryMinHeap
-  attr_accessor :store, :idx
+  attr_accessor :store, :idx, :prc
 
   def initialize(&prc)
     @store = []
     @idx = 0
+    @prc ||= Proc.new { |a, b| a <=> b }
   end
 
   def self.child_indices(len, idx)
@@ -17,21 +18,29 @@ class BinaryMinHeap
   end
 
   def push(num)
-    @store.push(num)
+    self.store.push(num)
+    self.store = BinaryMinHeap.heapify_up(self.store, self.idx, &self.prc)
+    self.idx += 1
   end
 
   def extract()
-    @store.shift
+    raise "nothing to extract " if self.store.length <= 0
+    return self.store.pop if self.store.length == 1
+
+    value = self.store[0]
+    self.store[0] = self.store.pop
+    self.store = BinaryMinHeap.heapify_down(self.store, 0, self.store.length, &self.prc)
+    return value
   end
 
   def self.heapify_up(arr, idx, &prc)
-    prc ||= Proc.new {|a, b| a > b}
+    prc ||= Proc.new {|a, b| a <=> b}
     item = arr[idx]
     return arr if idx <= 0
     parent_idx = parent_index(idx)
 
     parent = arr[parent_idx]
-    if prc.call(parent, item)#parent > item
+    if prc.call(parent, item) > 0#parent > item
       arr[idx], arr[parent_idx] = arr[parent_idx], arr[idx]
       return self.heapify_up(arr, parent_index(idx), &prc)
     end
@@ -39,28 +48,23 @@ class BinaryMinHeap
   end
 
   def self.heapify_down(arr, idx, length=arr.length, &prc)
+    prc ||= Proc.new {|a, b| a <=> b }
     return arr if idx >= length -1
-    left_i, right_i = child_indices(length, idx)
+    children = {}
+    indices = child_indices(length, idx)
+    indices.each {|idx| children[idx] = arr[idx] }
     el = arr[idx]
-    left = arr[left_i]
-    right = arr[right_i]
-    return arr if el < left && el < right
-    if left < right
-      arr[left_i], arr[idx] = arr[idx], arr[left_i]
-      return heapify_down(arr, left_i, &prc)
-    else
-      arr[right_i], arr[idx] = arr[idx], arr[right_i]
-      return heapify_down(arr, right_i, &prc)
+    return arr if children.none? {|idx, child| prc.call(child, el) < 0 } # Return array if no children < element
+    if indices.count == 1
+      swap_i = indices[0]
+    else # Swap with the smaller of the two elements.
+      left = indices[0]
+      right = indices[1]
+      swap_i = ( prc.call(children[left], children[right]) < 0) ? left : right
     end
 
-  end
-
-  def self.swap!(arr, idx1, idx2)
-    swap = arr[idx1]
-    arr[idx1] = arr[]
-  end
-
-  def heapify_down
+    arr[swap_i], arr[idx] = arr[idx], arr[swap_i]
+    return heapify_down(arr, swap_i, &prc)
   end
 
 end
