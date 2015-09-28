f8ec07a7f9efe1e6a59321ff9d5fedd9db3185ba
App Academy Student <student@appacademy.io>
2015-09-28 12:34:55 -0700

    Add Singly Linked List Link class methods

---
+++ code/lib/02a_link.rb
@@ -0,0 +1,34 @@
+class Link
+  attr_accessor :val, :prev, :next
+  def initialize(val)
+    @val = val
+    @prev = nil
+    @next = nil
+  end
+
+  def insert_left(link)
+    if self.prev
+      self.prev.next = link
+      link.prev = self.prev
+    end
+    link.next = self
+    self.prev = link
+  end
+
+  def insert_right(link)
+    if self.next
+      self.next.prev = link
+      link.next = self.next
+    end
+    self.next = link
+    link.prev = self
+  end
+
+  def remove()
+    self.prev.next = self.next
+    self.next.prev = self.prev
+    self.next = nil
+    self.prev = nil
+  end
+
+end
---
+++ code/lib/04c_quick_sort.rb
@@ -0,0 +1,8 @@
+class QuickSort
+  def self.partition(arr, idx1, idx2)
+  end
+
+  def self.sort2!(arr)
+  end
+
+end
